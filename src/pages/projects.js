2342138d86a81704d6ad05b3b7e795fa3f197302
bajunkin <bajunkin@ncsu.edu>
2020-02-08 18:56:40 -0500

    Cleaned up styling and color scheme

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,11 +13,13 @@ export default () => (
   <Layout>
     <h1><span role='img'>My Projects 📁</span></h1>
     <p>Checkout some of the stuff I've been working on.</p>
-      <div class="c" style={{outline: "5px solid #FFC145"}}>
-        <ListLink to="/tunechef/">TuneChef👨🏽‍🍳🎧</ListLink>
-        <p class="a">
-          WebApp that intelligently builds Spotify playlists for your party.
-        </p>
+      <div class="c" style={{}}>
+        <div class="cc">
+          <ListLink to="/tunechef/">TuneChef👨🏽‍🍳🎧</ListLink>
+          <p class="a">
+            WebApp that intelligently builds Spotify playlists for your party.
+          </p>
+        </div>
       </div>
   </Layout>
 )
