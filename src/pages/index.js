f64b61069c2798d9955fad0781502ed44bdd5110
bajunkin <bajunkin@ncsu.edu>
2020-07-16 14:40:57 -0400

    Resume update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import me from "../images/me.png"
+import me from "../images/me2.png"
 import yos from "../images/yos2.png"
 import { Link } from "gatsby"
 
@@ -15,25 +15,21 @@ export default () => (
     <Layout>  
         <h1>
           Hi! I'm Bryce 🙋🏻‍♂️
-          
         </h1>
         <img class="b" src={me} alt="TC" />
         <p> </p>
         <p>
-          I am a Junior studying computer science at NCSU (<strong>GO PACK</strong> <span role='img'>🐺</span>).
-          For <strong>7 months</strong> of 2019 I was lucky enough to intern at <strong>Nokia</strong> in the Bay Area,
-          where I collaborated with QA professionals to maintain their test infrastructure. Exposure to a QA system
-          at the size and complexity Nokia's gave me an appreciation for thorough and scalable testing that
-          I didn't have before my internship. 
+          I am a Senior studying CS at NCSU (<strong>GO PACK</strong> <span role='img'>🐺</span>).
+          I chose Software because it is a space where simple ideas can quickly and dramatically change the world <span role='img'>🌎</span>.
+          I've observed a modern motif characterized by random kids with internet access making industry-changing contributions (sometimes accidentally).
+          In a lot of ways, such as its youth and limited barriers to entry, the software industry feels like a <strong>new global frontier</strong>, 
+          and I'm excited to contribute to it <span role='img'>💻</span>.
         </p>
-        <p>
-          Being surrounded by the passionate people and high-energy culture leading software innovation was one
-          of the coolest experiences of my life and definitely had a lasting impact on my perspective of the industry. 
-          I am super excited to be a part of it.
-        </p>
-        <p>
-          This summer I will be interning at <strong>Cisco</strong> in Raleigh as a software engineer, where I hope to make tangible contributions
-          to my team as well as get my hands dirty with some new technology and tools I've never used before.
+        <p> 
+          This summer I'm interning at Cisco where I have become comfortable
+          building full stack applications at a professional level, from setting up relational databases to
+          designing and implementing REST apis to wireframing UIs. Thanks to this experience 
+          I have finally overcome the imposter syndrome that I think a lot rookies in this industry experience <span role='img'>😂</span>.
         </p>
 
         <p> </p>
@@ -43,22 +39,14 @@ export default () => (
         <img class="c" src={yos} alt="TC" />
         <p> </p>
         <p>
-          When I'm not coding I love hiking and discovering new music, so the Bay was a perfect fit for me.
-          Almost every weekend I was exploring the countless natural landmarks or seeing my favorite
-          artists live in San Francisco. 
+          Last year I was lucky enough to spend 7 months in the Bay Area while I was working at Nokia.
+          In addition to experiencing the cultural and technical epicenter of the software industry, I also
+          discovered some other passions of mine, such as live music <span role='img'>🎤</span> and hiking <span role='img'>🥾</span>. Every weekend I was exploring
+          one the area's countless natural landmarks or seeing my favorite artists live in San Francisco.
         </p>
         <p>
-
-          The Upper Yosemite Falls was easily one of my favorite hiking experiences, despite
-          almost not making it back due to a lack of preparation (long story). This trail is 
-          one-upped only by the Pikchu trail I climbed in Peru a year ago, which winds up the
-          mountain Pikchu for a cinematic view of the city Cusco.
-
-        </p>
-
-        <p>
-          At the start of this summer I'm doing a tour of 11 European contries, hopefully my hiking
-          portfolio will be a little longer by the end!
+          I definitely fell in love with the West coast, so I'm planning to move out to the Bay after graduating Spring 2021! 
+          This time around I'm gonna make sure to hit some of the famous music festivals and also scale the Half Dome at Yosemite.
         </p>
     </Layout>
   </div>
